49d0fb2912719f021bfec5f251400e74ee70f832
Zach Pratt <zppratt@gmail.com>
2022-11-13 16:22:33 -0500

    basic form elements in place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
     BrowserModule,
   ],
   providers: [],
