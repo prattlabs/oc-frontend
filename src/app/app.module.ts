6d3c82aa5368e0dde9c42a298d75a951692e05fd
Zach Pratt <zppratt@gmail.com>
2022-11-14 13:34:51 -0500

    token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { FormsModule } from '@angular/forms';
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
-    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
     BrowserModule,
     BrowserAnimationsModule
   ],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl,FormGroup, Validators} from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,29 +8,65 @@ import {FormControl,FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(private http: HttpClient) { }
+
+  angForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
 
   hide = true
-  email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('', [Validators.required]);
-  
+  submitFailed = false
+
+  get email(): any {
+    return this.angForm.get('email');
+  }
+  get password(): any {
+    return this.angForm.get('password');
+  }
+
   getEmailErrorMessage() {
-    if (this.email.hasError('required')) {
+    if (this.angForm.get("email")!.hasError('required')) {
       return 'You must enter a value';
     }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+    return this.angForm.get("email")!.hasError('email') ? 'Not a valid email' : '';
   }
 
   getPasswordErrorMessage() {
-    if (this.email.hasError('required')) {
+    if (this.angForm.get("password")!.hasError('required')) {
       return 'You must enter a value';
     }
 
-    return this.email.hasError('password') ? 'Password was incorrect' : '';
+    return this.angForm.get("password")!.hasError('password') ? 'Password was incorrect' : '';
   }
 
   Submit() {
     console.log("Submit");
+    console.log(this.angForm.get("email"));
+    console.log(this.angForm.get("password"));
+    this.http
+      .post('http://localhost:8080/login', {
+        username: this.angForm.get("email")!.value,
+        password: this.angForm.get("password")!.value,
+        token: this.getToken(),
+      }).subscribe({
+        next: (response) => {
+          console.log(response);
+          this.submitFailed = false;
+          window.location.href = "http://onecause.com/"
+        },
+        error: (error) => {
+          console.log(error);
+          this.submitFailed = true;
+        },
+      });
+  }
+
+  getToken(): number {
+    const now = new Date();
+    var strNow = "" + now.getHours() + "" + now.getMinutes()
+    return +strNow;
   }
 
 }
